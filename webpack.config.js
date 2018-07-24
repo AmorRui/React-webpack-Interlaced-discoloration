4f6d1e473d9d1ae0fdd0c7b87610a7049721d095
Amor <teng0009@126.com>
2018-07-24 10:54:50 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  // 默认情况下 webpack只能处理js文件，要想处理其他文件要配置loader
+  // 配置loader
   module:{
     // 配置文件解析的规则
     rules: [
@@ -25,10 +25,10 @@ module.exports = {
       // 解析scss文件  安装sass-loader 需要依赖 node-sass
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       // 字体文件用file-loader
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
       // 图片  file-loader  url-loader 两个loader都可以解析， url-loader 依赖于file-loader 
       // file-loader 不会对图片进行处理， url-loader会对图片进行base64编码  可以使用limit参数限制
       { test: /\.(png|bmp|jpg|jpeg)$/, use: 'url-loader?limit=1024' },
-      // webpack默认只能编译  es5的语法  
       // 安装babel-loader的命令  npm i babel-core babel-loader babel-plugin-transform-runtime --save
       // 安装preset字典   npm i babel-preset-env babel-preset-stage-0 --save
       // 还需要进行.babelrc 文件的配置
