28d130fa2a281eed4ee2329443c710728e6bd6a2
Amor <teng0009@126.com>
2018-07-24 10:54:19 +0800

    Update webpack.config.publish.js

--- webpack.config.publish.js
+++ webpack.config.publish.js
@@ -25,7 +25,6 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'js/[name].js'
   },
-  // 默认情况下 webpack只能处理js文件，要想处理其他文件要配置loader
   module:{
     // 配置文件解析的规则
     rules: [
@@ -44,6 +43,7 @@ module.exports = {
         publicPath: '../'
       }) },
       // 字体文件用file-loader
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
       // 图片  file-loader  url-loader 两个loader都可以解析， url-loader 依赖于file-loader 
       // file-loader 不会对图片进行处理， url-loader会对图片进行base64编码  可以使用limit参数限制
       { test: /\.(png|bmp|jpg|jpeg)$/, use: 'url-loader?limit=1024&name=img/img-[hash:6].[ext]' },
@@ -64,7 +64,7 @@ module.exports = {
   },
   // 配置插件的节点
   plugins: [
-    // 这个插件除了能够将html文件进行打包  还能将生成的js文件自动注入到html文件的末尾
+    // 将html文件进行打包  将生成的js文件自动注入到html文件的末尾
     new htmlWebpackPlugin({
       // 指定要处理的html文件模板
       template: path.join(__dirname, 'index.html'),
@@ -77,11 +77,6 @@ module.exports = {
     }
     }),
     new cleanWebpackPlugin(['dist']),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendors',
-    //   filename: 'js/vendors.js'
-    // }),
-    // new webpack.optimize.UglifyJsPlugin(),
     new extractTextPlugin('css/styles.css'),
     new optimizeCss()
   ]
