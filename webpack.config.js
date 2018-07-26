5c47a73ccdf210e2bc263468453592d6456a287e
Amor <teng0009@126.com>
2018-07-26 16:24:08 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,85 @@
-// 涉及到路径问题  引入path
-var path = require('path');
-// 引入生成html的插件
-var htmlWebpackPlugin = require('html-webpack-plugin');
-// 引入热更新插件
-var webpack = require('webpack');
-var hot  = webpack.HotModuleReplacementPlugin;
-var sync = require('browser-sync-webpack-plugin');
+"use strict";
 
-module.exports = {
-// 指定入口文件
-  entry: path.join(__dirname, 'main.js'),
-// 指定出口文件
-  output: {
-    // 指定打包之后输出的路径
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  // 指定是  development  和  production 模式，none选择退出任何默认优化选项
-  mode: 'none',
-  // 配置loader
-  module:{
-    // 配置文件解析的规则
-    rules: [
-      // 解析css文件
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      // 解析scss文件  安装sass-loader 需要依赖 node-sass
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      // 字体文件用file-loader
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
-      // 图片  file-loader  url-loader 两个loader都可以解析， url-loader 依赖于file-loader 
-      // file-loader 不会对图片进行处理， url-loader会对图片进行base64编码  可以使用limit参数限制
-      { test: /\.(png|bmp|jpg|jpeg)$/, use: 'url-loader?limit=1024' },
-      // 安装babel-loader的命令  npm i babel-core babel-loader babel-plugin-transform-runtime --save
-      // 安装preset字典   npm i babel-preset-env babel-preset-stage-0 --save
-      // 还需要进行.babelrc 文件的配置
-      // 使用babel-loader时  需要下载preset env 里面包含es6及之前的所有语法 
-      { test: /\.js$/ , use:['babel-loader'], exclude: /node_modules/ }
+  const path = require('path')
+  const Webpack = require('webpack')
+  const HtmlWebpackPlugin = require("html-webpack-plugin");
+  const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+  const CopyWebpackPlugin = require('copy-webpack-plugin')
+  module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, './dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.less$/,
+          use: ["style-loader", "css-loader", "less-loader"],
+          exclude: path.resolve(__dirname, "node_modules")
+        },
+        {
+          test: /\.js$/,
+          loader: ['babel-loader'],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(jpg|png|jpeg|bmp|gif)$/,
+          loader: ['url-loader?limit=5124&name=./dist/images/[name].[ext][hash]']
+        },
+        {
+          test: /\.(jpg|png|jpeg|bmp|gif)$/,
+          loader: ['file-loader?name=./dist/assets/images/[name].[ext][hash]']
+        },
+        { test: /\.(ttf|woff|woff2|eot|svg)$/,
+          loader: ['file-loader?name=./dist/fonts/[name].[ext][hash]']
+        }
+      ]
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      // 当404 自动跳转回首页
+      historyApiFallback: true,
+      // 让命令中少一些输出
+      noInfo: true,
+      // 代理
+      proxy: {
+        // 只有/api开头的请求地址，才会被代理
+        // 如果请求地址是 /test/home,这种不以/api开头的，是不会被代理的
+        // 示例： /api/logout, 最终会变成  http://api.xxx.com/v1/logout
+        "/api/*": {
+          // 目标服务器地址
+          // target: "http://127.0.0.1:7001/",
+          target: 'http://api.botue.com/',
+          // 目标服务器地址是否是安全协议
+          secure: false,
+          // 是否修改来源, true时-->让目标服务器以为是webpack-dev-server发出的请求
+          changeOrigin: true,
+          // '/api/login' =>    target + '/login'
+          // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
+          pathRewrite: { "^/api": "/" }
+          // http://api.xxx.com/v1/login
+        },
+        contentBase: "./dist",
+        // 启用热替换属性
+        hot: true,
+        // 使用颜色
+        colors: true,
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+      new CopyWebpackPlugin([{
+        from: './src/assets',
+        to: './assets'
+      }])
     ]
-  },
-  // 对devserver进行配置
-  devServer:{
-    open: true,
-    hot: true,
-    port: 8877,
-    // 改变webpack默认打开的html
-    openPage: 'index.html'
-  },
-  // 配置插件的节点
-  plugins: [
-    // 这个插件除了能够将html文件进行打包  还能将生成的js文件自动注入到html文件的末尾
-    new htmlWebpackPlugin({
-      // 指定要处理的html文件模板
-      template: path.join(__dirname, 'index.html'),
-      // 指定生成的html文件的名称
-      filename: 'index.html'
-    }),
-    new hot(),
-    new sync({
-      host: 'localhost',
-      port: 3000,
-      server: {baseDir: ['dist']}
-    })
-  ]
-}
+  }
